08f7a54f6472ad34effbb89e44bafc82d9a25f0b
rafaelbcastilhos <rafaelbcastilhos@gmail.com>
2022-03-25 20:56:40 -0300

    add controle agvs

---
+++ src/agv.c
@@ -0,0 +1,156 @@
+#include "agv.h"
+
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "agv_data.h"
+#include "process_sync.h"
+
+void agv_inicializa(agv_t *self, unsigned int id) {
+    /* TODO: Adicionar código nesta função se necessário! */
+
+    self->id = id;
+    self->cont_lampadas = 0;
+    self->posicionado = false;
+    self->reciclar = false;
+
+    if (pthread_create(&self->thread, NULL, agv_executa, (void *)self) == 0) {
+        plog("[AGV %u] Inicializado\n", self->id);
+    } else {
+        plog("[AGV %u] Erro ao inicializar AGV\n", self->id);
+    }
+}
+
+void *agv_executa(void *arg) {
+    /* Recupera o argumento de entrada (agv_t). */
+    agv_t *self = (agv_t *)arg;
+
+    /* TODO: Implementar aqui o comportamento do AGV */
+
+    // Esse if é executado na inicialização, quando não há nenhum AGV
+    // posicionado. Ele difere do loop principal apenas pela proteção extra,
+    // necessária porque na inicialização, e apenas na inicialização, o
+    // posicionamento simultaneo de 2 AGVs pode acontecer
+    if (!sem_trywait(&semaphores.agv_posiciona)) {
+        // Garante que os AGVs se posicionem para cargas diferentes
+        pthread_mutex_lock(&mutexes.agv_reciclar_prox);
+        bool reciclar_prox = agv_data.reciclar_prox;
+        agv_data.reciclar_prox = !agv_data.reciclar_prox;
+        pthread_mutex_unlock(&mutexes.agv_reciclar_prox);
+
+        agv_posiciona(self, reciclar_prox);
+
+        // Garante que não haverão 2 posts no semáforo de disponibilidade dos
+        // AGVs, afinal este deve ser binário. Apenas o carrinho de reciclagem,
+        // que é o segundo a chegar, faz o post
+        if (reciclar_prox) {
+            sem_post(&semaphores.agvs_ambos_disponiveis);
+        }
+
+        if (self->reciclar) {
+            sem_wait(&semaphores.agv_transporta_reprovadas);
+        } else {
+            sem_wait(&semaphores.agv_transporta_aprovadas);
+        }
+        agv_transporta(self);
+        if (agv_data.total_lampadas_entregues == config.quantidade_lampadas) {
+            sem_post(&semaphores.finaliza_sistema);
+        }
+    }
+
+    while (true) {
+        // Posiciona assim que um carrinho sair para transporte
+        sem_wait(&semaphores.agv_posiciona);
+        agv_posiciona(self, agv_data.reciclar_prox);
+        sem_post(&semaphores.agvs_ambos_disponiveis);
+
+        if (self->reciclar) {
+            sem_wait(&semaphores.agv_transporta_reprovadas);
+        } else {
+            sem_wait(&semaphores.agv_transporta_aprovadas);
+        }
+        agv_transporta(self);
+        // Finalize o sistema se todas as lâmpadas já tiverem sido entregues
+        if (agv_data.total_lampadas_entregues == config.quantidade_lampadas) {
+            sem_post(&semaphores.finaliza_sistema);
+        }
+    }
+
+    pthread_exit(NULL);
+}
+
+void agv_posiciona(agv_t *self, bool reciclar) {
+    /* AGV se posiciona para que lâmpadas sejam colocadas no seu compartimento
+     * de carga. */
+    if (!self->posicionado && self->cont_lampadas == 0) {
+        self->reciclar = reciclar;
+        self->posicionado = true;
+
+        if (reciclar) {
+            plog(
+                "[AGV %u] Posicionado para carregamento com lâmpadas para "
+                "reciclagem\n",
+                self->id);
+        } else {
+            plog(
+                "[AGV %u] Posicionado para carregamento com lâmpadas aprovadas "
+                "no teste\n",
+                self->id);
+        }
+    } else {
+        plog("[AGV %u] Erro ao tentar posicionar AVG em uso\n", self->id);
+    }
+}
+
+void agv_insere(agv_t *self, lampada_t *lampada) {
+    /* TODO: Inserir uma lâmpada no compartimento de carga do AGV. */
+    self->cont_lampadas++;
+
+    agv_data.total_lampadas_recebidas++;
+
+    if (agv_data.total_lampadas_recebidas == config.quantidade_lampadas) {
+        sem_post(&semaphores.agv_transporta_reprovadas);
+        sem_post(&semaphores.agv_transporta_aprovadas);
+    }
+
+    if (self->cont_lampadas == config.capacidade_agv) {
+        sem_wait(&semaphores.agvs_ambos_disponiveis);
+        if (self->reciclar) {
+            sem_post(&semaphores.agv_transporta_reprovadas);
+        } else {
+            sem_post(&semaphores.agv_transporta_aprovadas);
+        }
+    }
+    plog("[AGV %u] Lâmpada inserida no compartimento de carga.\n", self->id);
+}
+
+void agv_transporta(agv_t *self) {
+    /* TODO: Transportar a carga de lâmpadas para o depósito. */
+
+    // Informa ao sistema qual carga (aprovadas/reprovadas) o próximo carrinho
+    // deverá carregar
+    agv_data.reciclar_prox = self->reciclar;
+    self->posicionado = false;
+
+    // Manda posicionar o próximo AGV
+    sem_post(&semaphores.agv_posiciona);
+
+    // Aumenta a contagem de lâmpadas entregues
+    pthread_mutex_lock(&mutexes.total_lampadas_entregues);
+    agv_data.total_lampadas_entregues += self->cont_lampadas;
+    pthread_mutex_unlock(&mutexes.total_lampadas_entregues);
+
+    self->cont_lampadas = 0;
+    plog("[AGV %u] Lâmpadas transportadas para o depósito.\n", self->id);
+}
+
+void agv_finaliza(agv_t *self) {
+    /* TODO: Adicionar código aqui se necessário! */
+
+    pthread_cancel(self->thread);
+    pthread_join(self->thread, NULL);
+    plog("[AGV %u] Finalizado\n", self->id);
+}
---
+++ src/controle_agvs.c
@@ -0,0 +1,70 @@
+#include <time.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include "controle_agvs.h"
+#include "process_sync.h"
+#include "agv_data.h"
+
+void controle_inicializa_agvs(controle_agvs_t * controle, unsigned int quantidade_agvs)
+{
+    /* TODO: Adicionar código nesta função se necessário! */
+    
+    // Inicializa o semáforo posiciona para a quantidade máxima de carrinhos posicionados
+    sem_init(&semaphores.agv_posiciona, 0, 2);
+    pthread_mutex_init(&mutexes.agv_reciclar_prox, NULL); // Inicializa mutex de controle para reciclagem do proximo agv
+
+    agv_data.reciclar_prox = 0;
+    agv_data.total_lampadas_recebidas = 0;
+
+    // Inicializa os semáforos que regulam o transporte
+    sem_init(&semaphores.agv_transporta_reprovadas, 0, 0);
+    sem_init(&semaphores.agv_transporta_aprovadas, 0, 0);
+    sem_init(&semaphores.agvs_ambos_disponiveis, 0, 0);
+
+    sem_init(&semaphores.finaliza_sistema, 0, 0); // Inicializa o semáforo que controla a finalização do sistema
+
+    controle->quantidade_agvs = quantidade_agvs;
+    controle->agvs = (agv_t *) malloc(sizeof(agv_t) * quantidade_agvs);
+    for (int i = 0; i < quantidade_agvs; i++)
+        agv_inicializa(&controle->agvs[i], i);
+
+    plog("[controle AGVs] Inicializado\n");
+}
+
+agv_t * controle_retorna_agv(controle_agvs_t * self, bool reciclagem)
+{
+    /* TODO: retornar o AGV posicionado para receber lâmpadas aprovadas no teste (se reciclar==false) 
+       ou rejeitadas (se reciclar==true); aguarda o posicionamento de AGVs se necessário */
+
+    // Espera até que ambos os AGVs estejam posicionados
+    sem_wait(&semaphores.agvs_ambos_disponiveis);
+    sem_post(&semaphores.agvs_ambos_disponiveis);
+    for (int i = 0; i < config.quantidade_agvs; i++) {
+        if (self->agvs[i].posicionado && self->agvs[i].reciclar == reciclagem) {
+            return &self->agvs[i];
+        }
+    }
+
+    plog("[controle AGVs] AGV Posicionado não encontrado\n");
+    return NULL;
+}
+
+void controle_finaliza_agvs(controle_agvs_t * controle)
+{
+    /* TODO: Adicionar código nesta função se necessário! */
+    // Destroi os semáforos que regulam o transporte
+    sem_destroy(&semaphores.finaliza_sistema);
+    sem_destroy(&semaphores.agv_transporta_reprovadas);
+    sem_destroy(&semaphores.agv_transporta_aprovadas);
+    sem_destroy(&semaphores.agvs_ambos_disponiveis);
+    sem_destroy(&semaphores.agv_posiciona);
+
+    pthread_mutex_destroy(&mutexes.agv_reciclar_prox); // Destroi o mutex que controla a reciclagem do proximo
+
+    for (int i = 0; i < controle->quantidade_agvs; i++)
+        agv_finaliza(&controle->agvs[i]);
+    free(controle->agvs);
+
+    plog("[controle AGVs] Finalizado\n");
+}
